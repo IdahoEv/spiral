0ed41b658badd1158f9f2bc7b044be42351f024c
Evan Dorn <evan@lrdesign.com>
2012-11-21 14:27:09 -0800

    spec and implement argument checking

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,11 +4,25 @@ describe "Spiral Integrations" do
   let :command do "ruby #{script} " end
 
   describe "when run without arguments" do
-
     it "should give a usage message" do
       `#{command}`.should =~ /USAGE/
     end
   end
+
+  describe "when run with a non-square argument" do
+    it "should complain that the argument is not valid" do
+      `#{command} 11`.should =~ /You must provide an integer that is a perfect square/
+    end
+    it "should complain that the argument is not valid" do
+      `#{command} a`.should =~ /You must provide an integer that is a perfect square/
+    end
+    it "should complain that the argument is not valid" do
+      `#{command} 122`.should =~ /You must provide an integer that is a perfect square/
+    end
+
+  end
+
+
 
 
 end
--- spiral.rb
+++ spiral.rb
@@ -6,6 +6,13 @@ spiral = Spiral.new
 if ARGV.length != 1
   puts "USAGE: ruby spiral.rb <value>"
   exit(1)
+else
+  arg = ARGV.first.to_i
+  if arg > 0 and spiral.perfect_square?(arg.to_i)
+  else
+    puts "You must provide an integer that is a perfect square"
+    exit(1)
+  end
 end
 
 
