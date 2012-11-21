5ab0f31b3590e3693b4cc1fa325e77c107b0e17d
Evan Dorn <evan@lrdesign.com>
2012-11-21 12:45:19 -0800

    Spec and implement center_coords to find the center of the array.

--- lib/spiral.rb
+++ lib/spiral.rb
@@ -4,4 +4,14 @@ class Spiral
     sqrt = Math.sqrt(num)
     sqrt == sqrt.to_i
   end
+
+  def center_coords(num)
+    sqrt = Math.sqrt(num)
+    [
+      ((sqrt/2.0)-0.5).to_i,
+      (sqrt/2.0).to_i
+    ]
+  end
+
+
 end
--- spec/spiral_spec.rb
+++ spec/spiral_spec.rb
@@ -13,6 +13,25 @@ describe Spiral do
     it "should return false for perfect squares" do
       non_squares.each { |num| spiral.perfect_square?(num).should be_false }
     end
-
   end
+
+  # Array is specified as [0,0] == top left
+  describe "center_coords" do
+    it "should return [0,0]  n == 1" do
+      spiral.center_coords(1).should == [ 0, 0 ]
+    end
+    it "should return [1,0]  n == 4" do
+      spiral.center_coords(4).should == [ 0, 1 ]
+    end
+    it "should return [1,1]  n == 9" do
+      spiral.center_coords(9).should == [ 1, 1 ]
+    end
+    it "should return [1,2]  n == 16" do
+      spiral.center_coords(16).should == [ 1, 2 ]
+    end
+    it "should return [1,2]  n == 25" do
+      spiral.center_coords(25).should == [ 2, 2 ]
+    end
+  end
+
 end
