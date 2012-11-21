0b63e1274fe40cb29ed33e32ffc24aa8f5fb1388
Evan Dorn <evan@lrdesign.com>
2012-11-21 12:49:47 -0800

    Refactor the center_coords method with Math.floor, add comment to specify how center is chosen.

--- lib/spiral.rb
+++ lib/spiral.rb
@@ -5,11 +5,14 @@ class Spiral
     sqrt == sqrt.to_i
   end
 
+
+  # The center of the 2D array, zero offset.  For even height/width arrays,
+  # center is the cell to the lower-left of the center vertex.
   def center_coords(num)
     sqrt = Math.sqrt(num)
     [
-      ((sqrt/2.0)-0.5).to_i,
-      (sqrt/2.0).to_i
+     ((sqrt/2.0)-0.5).floor,
+     (sqrt/2.0).floor
     ]
   end
 
